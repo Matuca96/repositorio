0e9ac964d93229e91313395ac2a1c4c27739635d
Matuca96 <matucagatao@gmail.com>
2025-03-23 16:33:15 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+num1 = float(input("inssira o primeiro numero:"))
+num2 = float(input("inssira o segundo numero"))
+
+if operator == "+":
+    result = num1 + num2
+    print(f"O resultado e {result}")
+elif operator == "-":
+    result = num1 + num2
+    print(f"O resultado e {result}")
+elif operator == "*":
+    result = num1 * num2
+    print(f"O resultado e {result}")
+elif operator == "/":
+    result = num1 / num2
+    print(f"O resultado e {result}")
+else:
+    print(f"o gerador{operator}nao e valido")                
